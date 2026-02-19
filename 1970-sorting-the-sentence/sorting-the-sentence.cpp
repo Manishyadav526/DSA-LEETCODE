1b77bb74000143865df5fbeac3c97273020d40d5
Manish Yadav <manishyadav92124@gmail.com>
2026-02-19 22:40:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (21.17%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.cpp
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        vector<string>ans(10);
-        string temp;
-        int count=0;//to check only the no.of words present no need to traverse upto 9 
-        int index=0;
+    int n=s.length();
+    unordered_map<int , string>mpp; //index and word
 
-        while(index<s.size()){
-
-            if(s[index]==' '){
-                int pos=temp[temp.size()-1]-'0';//for changing into int
-                temp.pop_back();
-                ans[pos]=temp;
-                temp.clear();
-                count++;
-                index++;
-
-            }
-            else{
-                temp=temp+s[index];
-                index++;
-            }
+    string word = "";
+    int index;
+    for(int i=0;i<n;i++){
+        if(s[i]==' '){
+            index=word[word.size()-1]-'0';
+            word.pop_back();
+            mpp[index]=word;
+            word="";
         }
-              int pos=temp[temp.size()-1]-'0';//for changing into int 
-              //for the last word which does not have any space
-                temp.pop_back();
-                ans[pos]=temp;
-                temp.clear();
-                count++;
+        else{
+            word=word+s[i];
+        }
 
-                for(int i=1;i<=count;i++){
-                    temp=temp+ans[i];
-                    temp=temp+" ";
-                }
-        temp.pop_back();
-        return temp;   
+    }
+    index=word[word.size()-1]-'0';
+    word.pop_back();
+    mpp[index]=word; //last word bhi dal diya map me
+
+    string ans="";
+    for(int i=1;i<=mpp.size();i++){
+        ans=ans+mpp[i]+" ";
+    }
+    ans.pop_back(); // Remove trailing space
+    return ans;     
     }
 };
