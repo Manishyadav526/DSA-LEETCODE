01e19af22beef90080aa7c0a97b25c6d5a2515a4
Manish Yadav <manishyadav92124@gmail.com>
2026-02-19 22:10:24 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (99.28%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        vector<int> lower(26, 0);
-        vector<int> upper(26, 0);
-            for (int i = 0; i < s.size(); i++) {
-            if (s[i] >= 'a')
-                lower[s[i] - 'a']++;
-            else
-                upper[s[i] - 'A']++;
-        }
-        int count = 0;
-        bool odd = 0;
+    int n=s.length();
 
-        for (int i = 0; i < 26; i++) {
-            if(lower[i] % 2==0){
-                count=count+lower[i];
-            }
-            else{
-                count=count+lower[i]-1;
-                odd=1;
-            }
-        }
+    vector<int>lower(26,0);
+    vector<int>upper(26,0);
 
-        // Count pairs from uppercase letters
-        for (int i = 0; i < 26; i++) {
-            if(upper[i] % 2==0){
-                count=count+upper[i];
-            }
-            else{
-                count=count+upper[i]-1;
-                odd=1;
-            }
+    for(int i=0;i<n;i++){
+        if(s[i]>='a'){
+            lower[s[i]-'a']++;
         }
-        return count+odd;
+        else{
+            upper[s[i]-'A']++;  
+        }
+    }
+    int count = 0;
+    bool odd=0;
+    for(int i=0;i<26;i++){
+        if(lower[i]%2==0){
+            count+=lower[i];
+        }
+        else{
+            count+=lower[i]-1;
+            odd=1;
+        }
+        if(upper[i]%2==0){
+            count+=upper[i];
+        }
+        else{
+            count+=upper[i]-1;
+            odd=1;
+        }
+    }
+    return odd+count;    
     }
 };
-
-
