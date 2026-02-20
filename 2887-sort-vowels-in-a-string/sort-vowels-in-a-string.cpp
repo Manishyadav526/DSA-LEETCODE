ee3cfaa8f98fdd0e996261e93cb44621e2b3d31f
Manish Yadav <manishyadav92124@gmail.com>
2026-02-20 20:50:10 +0530

    Time: 16 ms (54.76%) | Memory: 15.7 MB (83.42%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        string vowels = "";  // to store all vowels
-
+        string vowels = "";
         // Step 1: Collect vowels and replace them with a placeholder
         for (int i = 0; i < s.size(); i++) {
             if (isVowel(s[i])) {
-                vowels += s[i];   // store vowel
-                s[i] = '#';       // mark position
+                vowels += s[i];  
+                s[i] = '#';      
             }
         }
-
         // Step 2: Sort the vowels
         sort(vowels.begin(), vowels.end());
-
         // Step 3: Put sorted vowels back in original string
-        int j = 0; // index for vowels
+        int j = 0; 
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '#') {
                 s[i] = vowels[j];
                 j++;
             }
         }
-
         return s;
     }
-
     // Helper function to check vowel
     bool isVowel(char ch) {
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
