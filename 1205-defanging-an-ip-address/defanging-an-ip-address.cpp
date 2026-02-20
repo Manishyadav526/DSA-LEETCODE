b44c9db20bbac4fae4f98b9d7a60d7463509b883
Manish Yadav <manishyadav92124@gmail.com>
2026-02-20 20:02:42 +0530

    Time: 3 ms (26.68%) | Memory: 7.9 MB (12.49%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        string ans="";
-        for(int i=0;i<address.size();i++){
+     string ans="";
+     for(int i=0;i<address.length();i++){
         if(address[i]=='.'){
             ans=ans+"[.]";
         }
         else{
             ans=ans+address[i];
         }
-        }
-        return ans;
+    }
+    return ans;
     }
 };
