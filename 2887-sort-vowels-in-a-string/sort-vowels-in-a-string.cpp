69c96540798e14cb1606803a2503b54455e4110f
Manish Yadav <manishyadav92124@gmail.com>
2026-02-20 21:03:23 +0530

    Time: 15 ms (63.07%) | Memory: 15.7 MB (73.29%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        string vowels = "";
-        // Step 1: Collect vowels and replace them with a placeholder
-        for (int i = 0; i < s.size(); i++) {
-            if (isVowel(s[i])) {
-                vowels += s[i];  
-                s[i] = '#';      
-            }
+    string vowel="";
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+            vowel.push_back(s[i]);
+            s[i]='*';
         }
-        // Step 2: Sort the vowels
-        sort(vowels.begin(), vowels.end());
-        // Step 3: Put sorted vowels back in original string
-        int j = 0; 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '#') {
-                s[i] = vowels[j];
-                j++;
-            }
-        }
-        return s;
     }
-    // Helper function to check vowel
-    bool isVowel(char ch) {
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-            return true;
-        return false;
+    sort(vowel.begin(), vowel.end());
+    int index=0;
+    for(int j=0;j<s.length();j++){
+        if(s[j]=='*'){
+            s[j]=vowel[index++];
+        }
+    }
+    return s;
     }
 };
+  
