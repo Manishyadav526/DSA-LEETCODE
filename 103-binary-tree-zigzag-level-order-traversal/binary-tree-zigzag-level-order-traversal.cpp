750ffc4170769cde0573d5e72f688f2777a4b601
Manish Yadav <manishyadav92124@gmail.com>
2026-04-04 21:32:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (77.40%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
-        vector<vector<int>> ans;
-        if(!root) return ans;
-
-        queue<TreeNode*> qt;
-        qt.push(root);
-
-        bool leftToRight = true;
-
-        while(!qt.empty()){
-
-            int size = qt.size();
-            vector<int> level(size);
-
-            for(int i = 0; i < size; i++){
-
-                TreeNode* cur = qt.front();
-                qt.pop();
-
-                int index;
-
-                if(leftToRight)
-                    index = i;
-                else
-                    index = size - 1 - i;
-
-                level[index] = cur->val;
-
-                if(cur->left) qt.push(cur->left);
-                if(cur->right) qt.push(cur->right);
-            }
-
-            leftToRight = !leftToRight;
-            ans.push_back(level);
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root){
+        vector<vector<int>>ans;
+        if(!root){
+            return ans;
         }
+        queue<TreeNode*>qt;
+        qt.push(root);
+        bool reverse=false;
+        while(!qt.empty()){
+            int size=qt.size();
+            vector<int>level(size);
 
+            for(int i=0;i<size;i++){
+            TreeNode* cur=qt.front();
+            qt.pop();
+
+            int index;
+            if(reverse){
+                index=size-1-i;
+            }
+            else{
+                index=i; 
+            }
+            level[index]=cur->val;
+
+            if(cur->left){
+                qt.push(cur->left);
+            }
+            if(cur->right){
+                qt.push(cur->right);
+            }
+        }
+        reverse=!reverse;
+        ans.push_back(level);
+        }
         return ans;
     }
 };
