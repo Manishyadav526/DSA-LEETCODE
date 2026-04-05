d2e186dbad12ba96acf5964e9c072c49c15ee043
Manish Yadav <manishyadav92124@gmail.com>
2026-04-05 11:34:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -12,37 +12,56 @@ public:
 };
 */
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> bottomView(Node *root) {
-        vector<int> ans;
-        if (root == NULL) 
-            return ans;
 
-        map<int, int> mpp;
-        queue<pair<Node*, int>> q;
+    void find(Node* root, int pos, int &l, int &r) {
+        if(!root) return;
 
-        q.push({root, 0});
+        l = min(l, pos);
+        r = max(r, pos);
 
-        while (!q.empty()) {
-            auto p = q.front();
+        find(root->left, pos - 1, l, r);
+        find(root->right, pos + 1, l, r);
+    }
+
+    vector<int> bottomView(Node *root)
+    {
+        int l = 0, r = 0;
+        find(root, 0, l, r);
+
+        vector<int> ans(r - l + 1);
+
+        queue<Node*> q;
+        queue<int> index;
+
+        q.push(root);
+        index.push(-1 * l);
+
+        while(!q.empty())
+        {
+            Node* temp = q.front();
             q.pop();
 
-            Node* node = p.first;
-            int line = p.second;
+            int pos = index.front();
+            index.pop();
 
-            mpp[line] = node->data;
+            // 👇 Only change: Always overwrite
+            ans[pos] = temp->data;
 
-            if (node->left != NULL)
-                q.push({node->left, line - 1});
+            if(temp->left) {
+                q.push(temp->left);
+                index.push(pos - 1);
+            }
 
-            if (node->right != NULL)
-                q.push({node->right, line + 1});
+            if(temp->right) {
+                q.push(temp->right);
+                index.push(pos + 1);
+            }
         }
-
-        for (auto it : mpp)
-            ans.push_back(it.second);
 
         return ans;
     }
 };
+
