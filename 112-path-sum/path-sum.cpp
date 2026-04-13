b362a520b8286796ec16e1f26d37a806b7a24571
Manish Yadav <manishyadav92124@gmail.com>
2026-04-14 01:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (94.89%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,38 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool checkpath(TreeNode* root , int targetSum , int &sum){
-        if(!root){
-            return 0;
-        }
-        sum=sum+root->val;
+
+    bool checkpath(TreeNode* root, int targetSum, int sum) {
+        if (!root) return false;
+
+        sum += root->val;
 
         if (!root->left && !root->right) {
-        bool ok = (sum == targetSum);
-        sum -= root->val;   // backtrack before return
-        return ok;
+            return sum == targetSum;
         }
-        bool left=checkpath(root->left, targetSum , sum);
-        bool right=checkpath(root->right,targetSum, sum);
-        
-        sum=sum-root->val;
-        return left||right;
+
+        return checkpath(root->left, targetSum, sum) ||
+               checkpath(root->right, targetSum, sum);
     }
+
     bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum=0;
-        if(!root){
-            return 0;
-        }
-        return checkpath(root , targetSum , sum); 
+        return checkpath(root, targetSum, 0);
     }
 };
