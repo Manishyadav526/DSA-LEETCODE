06b7c60659d57df8079c37a166fc49f76f64d64a
Manish Yadav <manishyadav92124@gmail.com>
2026-05-20 13:34:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (99.82%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int open=0;
+        int close=0;
+        int maxLen=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                open++;
+            }else{
+                close++;
+            }
+            if(open==close){
+                maxLen=max(maxLen,open+close);
+            }else if(close>open){
+                open=close=0;
+            }
+        }
+        open=0;
+        close=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='('){
+                open++;
+            }else{
+                close++;
+            }
+            if(open==close){
+                maxLen=max(maxLen,open+close);
+            }else if(open>close){
+                open=close=0;
+            }
+        }
+        return maxLen;
+    }
+};
